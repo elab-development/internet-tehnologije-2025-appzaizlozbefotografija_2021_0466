52d6e0c409348b8f1b28f0d68511f13f92f56727
Pavle Srdanovic <srdanovicpavle@gmail.com>
2026-02-05 00:59:18 +0100

    Izmena App

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Prijava from "./pages/Prijava";
 import Izlozbe from "./pages/Izlozbe";
+import Pocetna from "./pages/Pocetna";
+import IzlozbaDetalji from "./pages/IzlozbaDetalji";
+import Registracija from "./pages/Registracija";
+import Galerija from "./pages/Galerija";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
-        <Link to="/">Izložbe</Link>
-      </nav>
+      <NavBar />
+
 
       <div style={{ padding: "16px" }}>
         <Routes>
-          <Route path="/" element={<Izlozbe />} />
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/izlozbe/:id/prijava" element={<Prijava />} />
+          <Route path="/registracija" element={<Registracija />} />
+          <Route path="/izlozbe" element={<Izlozbe />} /> 
+          <Route path="/izlozbe/:id" element={<IzlozbaDetalji />} /> 
+          <Route path="/izlozbe/:id/galerija" element={<Galerija />} />
         </Routes>
       </div>
     </BrowserRouter>
